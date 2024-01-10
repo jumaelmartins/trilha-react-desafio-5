1ba0fb9528e452e5fd2b853336a1b184afafdfa2
jumaelmartins <jumaelmartins@gmail.com>
2024-01-10 16:56:22 -0300

    :sparkles: feat: Adicionado pagina de Detalhes para cada Post

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://ryczgwcdhpeemytlqceg.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5Y3pnd2NkaHBlZW15dGxxY2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQxNTY4NDcsImV4cCI6MjAxOTczMjg0N30.w6dmqZ-DP720HGlZi07Zzjs_XW9G4eHfQgvCz6ufwFs",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5Y3pnd2NkaHBlZW15dGxxY2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQxNTY4NDcsImV4cCI6MjAxOTczMjg0N30.w6dmqZ-DP720HGlZi07Zzjs_XW9G4eHfQgvCz6ufwFs"
     }
 })
